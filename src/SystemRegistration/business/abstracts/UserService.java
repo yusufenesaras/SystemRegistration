32c1056ae1056ba708a38677ac9076ae3064e6f1
Yusuf Enes <58237566+y-enesaras@users.noreply.github.com>
2021-05-07 16:46:05 +0300

    Update UserService.java

--- src/SystemRegistration/business/abstracts/UserService.java
+++ src/SystemRegistration/business/abstracts/UserService.java
@@ -7,8 +7,8 @@ import SystemRegistration.entities.concretes.User;
 public interface UserService {
 	
 	void register(User user);
-    void login(User user);
-    void delete(User user);
-    User getByMail(String mail);
+        void login(User user);
+        void delete(User user);
+        User getByMail(String mail);
 	List<User> getAll();
 }
